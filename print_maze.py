c50dd7e5374e0431ae397d721b40805caf5acb34
Thrystan01204 <47844397+Thrystan01204@users.noreply.github.com>
2023-07-23 22:18:07 +0400

    Externalize some functions

---
+++ print_maze.py
@@ -0,0 +1,32 @@
+def print_closed_h_walls(width):
+    for _ in range(width):
+        print("+---", end="")
+    print("+")
+
+
+def print_h_walls(width, line, h_walls):
+    for j in range(width):
+        if h_walls[width * line + j]:
+            print("+---", end="")
+        else:
+            print("+   ", end="")
+    print("+")
+
+
+def print_v_walls(width, line, v_walls):
+    print("|", end="")
+    for j in range(width - 1):
+        if v_walls[(width - 1) * line + j]:
+            print("   |", end="")
+        else:
+            print("    ", end="")
+    print("   |")
+
+
+def print_grid(width, height, v_walls, h_walls):
+    print_closed_h_walls(width)
+    for i in range(height - 1):
+        print_v_walls(width, i, v_walls)
+        print_h_walls(width, i, h_walls)
+    print_v_walls(width, height - 1, v_walls)
+    print_closed_h_walls(width)
